aa5a94e6cb0a42e18973be23994b4024ec07f24d
Robert Servado <servadorobert@gmail.com>
2023-03-11 22:45:44 -0500

    Modified ProductsPage component and added more detail and loop over the fake-data.js file

--- fsv-front-end/src/views/ProductsPage.vue
+++ fsv-front-end/src/views/ProductsPage.vue
@@ -1,13 +1,25 @@
 <template>
-  <h1>{{ title }}</h1>
+  <div id="page-wrap">
+    <div class="grid-wrap">
+      <div v-for="product in products" :key="product.id" class="product-item">
+        <img :src="product.imageUrl" :alt="product.name" />
+        <h3 class="product-name">{{ product.name }}</h3>
+        <p class="product-price">${{ product.price }}</p>
+        <router-link v-bind:to="'/products/' + product.id">
+          <button>View Details</button>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { products } from "../fake-data";
 export default {
   name: "ProductsPage",
   data() {
     return {
-      title: "Hello Everyone!",
+      products,
     };
   },
 };
