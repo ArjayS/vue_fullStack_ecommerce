653a623bedbc55ed3d046caa178d5909cd613a0b
Robert Servado <servadorobert@gmail.com>
2023-03-13 20:59:42 -0400

    Modified App.vue to be able to display the NavBar component

--- fsv-front-end/src/App.vue
+++ fsv-front-end/src/App.vue
@@ -1,13 +1,19 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
+    <NavBar />
     <router-view />
   </div>
 </template>
 
+<script>
+import NavBar from "./components/NavBar.vue";
+export default {
+  name: "App",
+  components: {
+    NavBar,
+  },
+};
+</script>
 <!-- Did not used scoped to be able to implement the styling verywhere in our app -->
 <style>
 * {
