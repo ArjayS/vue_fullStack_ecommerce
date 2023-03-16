3d0c8a40497a64588abf77a7181b8d9122efe1b0
Robert Servado <servadorobert@gmail.com>
2023-03-15 21:00:59 -0400

    Modified serverjs and add notes

--- fsv-back-end/src/server.js
+++ fsv-back-end/src/server.js
@@ -4,18 +4,22 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(bodyParser.json());
 
+// From Postman activating the res.send through a GET request of localhost:8000/hello
 app.get("/hello", (req, res) => {
   res.send("Hello!");
 });
 
+// From Postman activating the res.send through a POST of request localhost:8000/hello
 app.post("/hello", (req, res) => {
   res.send(`Hello ${req.body.name}`);
 });
 
+// From Postman activating the res.send through a GET request of localhost:8000/hello/Robert and adding a parameter of Robert
 app.get("/hello/:name", (req, res) => {
   res.send(`Hello ${req.params.name}!`);
 });
 
+// Server listening to PORT = 8000
 app.listen(8000, () => {
   console.log("Server is listening on port 8000");
 });
