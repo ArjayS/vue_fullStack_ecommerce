d090e9de66a8d31ed9d75c7167bdaba52b7cbc52
Robert Servado <servadorobert@gmail.com>
2023-03-16 19:36:21 -0400

    Modified server.js and a fifth endpoint that removes an item in the cart

--- fsv-back-end/src/server.js
+++ fsv-back-end/src/server.js
@@ -165,7 +165,7 @@ app.post("/api/users/:userId/cart", (req, res) => {
   }
 });
 
-// @2:00:10, Fifth endpoint is for removing items in the cart
+// @2:00:10, Fifth endpoint is for removing item in the cart
 app.delete("/api/users/:userId/cart/:productId", (req, res) => {
   const { productId } = req.params;
   cartItems = cartItems.filter((product) => product.id !== productId);
