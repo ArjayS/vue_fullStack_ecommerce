326cbd6335ea0fd285e2983ac549213460c5756a
Robert Servado <servadorobert@gmail.com>
2023-03-19 18:22:41 -0400

    Modified ProductsPage.vue and added some code to attempt to make an Axios Call

--- fsv-front-end/src/views/ProductsPage.vue
+++ fsv-front-end/src/views/ProductsPage.vue
@@ -5,7 +5,10 @@
 </template>
 
 <script>
-import { products } from "../fake-data";
+// @2:41:15, Using Axios in the ProductsPage.vue view page
+import axios from "axios";
+
+// import { products } from "../fake-data"; <--- Using the fake-data.js file
 import ProductsGrid from "../components/ProductsGrid.vue";
 export default {
   name: "ProductsPage",
@@ -14,8 +17,13 @@ export default {
   },
   data() {
     return {
-      products,
+      products: [],
     };
+  },
+  async created() {
+    const result = await axios.get("http://localhost:8000/api/products");
+    const products = result.data;
+    this.products = products;
   },
 };
 </script>
