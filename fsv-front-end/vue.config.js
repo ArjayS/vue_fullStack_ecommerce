c9353b485281bdcb3367ef5a310e77c8f8eab4e6
Robert Servado <servadorobert@gmail.com>
2023-03-21 21:27:33 -0400

    Modified vue.config.js

--- fsv-front-end/vue.config.js
+++ fsv-front-end/vue.config.js
@@ -1,9 +1,5 @@
-const { defineConfig } = require("@vue/cli-service");
-(module.exports = {
+module.exports = {
   devServer: {
     proxy: "http://localhost:8000",
   },
-}),
-  defineConfig({
-    transpileDependencies: true,
-  });
+};
